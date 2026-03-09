b86a765bb19b62b849e27d5a49e5db0885c8ce51
fanbook-wangdage <124357765+fanbook-wangdage@users.noreply.github.com>
2026-03-09 19:32:15 +0800

    添加sentry

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,28 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import '@/styles/index.scss'
-
+import '@/router/permission'
 import App from './App.vue'
 import router from './router'
 import { useThemeStore } from './stores/theme'
 import Particles from '@tsparticles/vue3'
 import { loadSlim } from '@tsparticles/slim'
 import type { Engine } from '@tsparticles/engine'
-
+import * as Sentry from "@sentry/vue";
 
 const app = createApp(App)
 const pinia = createPinia()
+
+Sentry.init({
+  app,
+  dsn: "https://2e38c08821de95d002b6e6253d3cd599@o4507525750521856.ingest.us.sentry.io/4511014276169728",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 0.2,
+  tracePropagationTargets: ["localhost", "https://htserver.wdg.cloudns.ch/"],
+});
 
 app.use(pinia)
 app.use(router)
@@ -24,7 +35,6 @@ app.use(Particles, {
     await loadSlim(engine)
   }
 })
-import '@/router/permission'
 
 // 初始化主题
 const themeStore = useThemeStore(pinia)
